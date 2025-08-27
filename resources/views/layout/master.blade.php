7b0a0ca079d8000e953daf9474090f76710dc4f0
Ludy Hasby Aulia <fewesgalih@gmail.com>
2025-08-27 21:44:22 +0700

    add nav active bar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -16,10 +16,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+          <a class="nav-link {{request()->routeIs(patterns: 'home') ? 'active' : ''}}" aria-current="page" href="{{route('home')}}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{route('show')}}">List</a>
+          <a class="nav-link {{request()->routeIs('show') ? 'active' : ''}}" href="{{route('show')}}">List</a>
         </li>
       </ul>
     </div>
